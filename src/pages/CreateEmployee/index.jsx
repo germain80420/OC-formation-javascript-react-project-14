0d98bc814cf1e92bc97d2cbe7082721314477cf0
Germain Bonnard <germain80420@gmail.com>
2024-04-03 17:25:03 +0200

    mise en commentaires du code de generation employees

--- src/pages/CreateEmployee/index.jsx
+++ src/pages/CreateEmployee/index.jsx
@@ -1,14 +1,14 @@
 import { NavLink } from "react-router-dom"
 import FormEmployee from "../../components/FormEmploye"
 
-function CreateEmployee(){
-    return(
-        <div>
-            <h1>HRnet</h1>
-            <NavLink to="/employeesList">View Current Employees</NavLink>
-            <h2>Create Employee</h2>
-            <FormEmployee/>
-        </div>
-    )
-} 
+function CreateEmployee() {
+  return (
+    <div>
+      <h1>HRnet</h1>
+      <NavLink to="/employeesList">View Current Employees</NavLink>
+      <h2>Create Employee</h2>
+      <FormEmployee />
+    </div>
+  )
+}
 export default CreateEmployee
